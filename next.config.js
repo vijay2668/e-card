c965cf2d600db5a959ad813fae3851ca46112a3e
vijay2668 <121243884+vijay2668@users.noreply.github.com>
2023-04-30 17:44:58 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['firebasestorage.googleapis.com']
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      // configure firebase modules to be loaded only on the client-side
+      config.resolve.fallback = {
+        fs: false,
+        net: false,
+        tls: false,
+        child_process: false,
+      };
+    }
+    return config;
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 }
 
 module.exports = nextConfig
