1438aa16f6e36172ee1726f1850b221fd93e08a7
vijay2668 <121243884+vijay2668@users.noreply.github.com>
2023-05-01 08:53:20 +0530

    Update index.jsx

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,5 +1,5 @@
 "use client ";
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "../../styles/login.module.css";
 import { useAuthState } from "react-firebase-hooks/auth";
 import {
