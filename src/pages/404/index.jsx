931f6f18c3515d64c828a40df77802101778a92a
vijay2668 <121243884+vijay2668@users.noreply.github.com>
2023-05-01 08:54:26 +0530

    Update index.jsx

--- src/pages/404/index.jsx
+++ src/pages/404/index.jsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
-import React, { useState } from "react";
+import { useState } from "react";
 
 const NotFound = () => {
   const router = useRouter();
